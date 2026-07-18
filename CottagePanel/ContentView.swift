a04bcfa70336b811299f45c0abdcdaecb91935b0
hjy_666 <hjy_233@qq.com>
2026-07-18 18:22:54 +0800

    Initial Commit

---
+++ CottagePanel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CottagePanel
+//
+//  Created by hjy_666 on 2026/7/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
